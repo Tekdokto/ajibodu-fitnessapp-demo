7d6295e8c9372fc55701517c2c9b84e27950ba8e
RaphaelAjibodu <Raphael.Ajibodu@stanbicibtc.com>
2023-02-06 21:07:00 +0100

    updated

--- src/@core/components/auth/AuthGuard.tsx
+++ src/@core/components/auth/AuthGuard.tsx
@@ -26,11 +26,11 @@ const AuthGuard = (props: AuthGuardProps) => {
       if (auth.user === null && !window.localStorage.getItem('userData')) {
         if (router.asPath !== '/') {
           router.replace({
-            pathname: '/login',
+            pathname: '/dashboards/activities',
             query: { returnUrl: router.asPath }
           })
         } else {
-          router.replace('/login')
+          router.replace('/dashboards/activities')
         }
       }
     },
